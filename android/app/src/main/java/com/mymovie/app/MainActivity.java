dc7595abc7172e766602ea7d7ab5daca619fc5f9
adnanganie <ganieadnan10@gmail.com>
2021-05-20 17:14:59 +0530

    Android Platform added

---
+++ android/app/src/main/java/com/mymovie/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mymovie.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mymovie.app',
+  appName: 'movieApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
